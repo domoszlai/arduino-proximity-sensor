a3c5e36c81b6a806fe1884086359f7276abe4c5c
Laszlo Domoszlai <l.domoszlai@science.ru.nl>
2017-03-27 10:04:08 +0200

    fix comments

--- proximity_sensor_test.ino
+++ proximity_sensor_test.ino
@@ -4,9 +4,13 @@
 
 #define DEBUG
 
-#define MAX_FREQUENCY_DROP        500
+// It should be measured, but it is more or less all right for 100pF and 200K
+#define MAX_FREQUENCY_DROP        600
+// Less noisy -> go closer to 0
 #define MAX_EXPECTED_FREQ_NOISE   30
+// Less noisy -> go closer to 0
 #define CL_WEIGHT                 0.75
+// Less noisy -> go closer to 1
 #define BL_WEIGHT                 0.995
 
 float current_line = 0;
@@ -30,9 +34,11 @@ void loop()
   current_line = current_line * CL_WEIGHT + (1 - CL_WEIGHT) * f;
   baseline = baseline * BL_WEIGHT + (1 - BL_WEIGHT) * f;
 
-  // distance is reciprocal, bigger value indicates smaller distance
-  // and its range is 0-255 
+  // The difference is reciprocal, the bigger indicates smaller distance
+  // as frequency drops with proximity
   int diff = max(0, baseline - current_line);
+  
+  // Force threshold to avoid flickering around 0
   if(diff < MAX_EXPECTED_FREQ_NOISE)
   { 
     diff = 0;
@@ -41,6 +47,8 @@ void loop()
   {
     diff -= MAX_EXPECTED_FREQ_NOISE;
   }
+
+  // Convert the difference to range [0-255]
   int distance = (diff) * 255L / MAX_FREQUENCY_DROP;
   distance = min(distance, 255);
 
